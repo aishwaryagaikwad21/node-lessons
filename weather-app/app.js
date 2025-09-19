3bd7252c2615e42894743181124bfbb124af2a0a
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2025-09-19 16:31:36 +0530

    geocode and display weather

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,25 @@
 const request = require('postman-request');
 
-const url = 'http://api.weatherstack.com/current?access_key=ea797f0c26e77f60ee87ae3b81fd4f49&query=37.8267,-122.4233'
+const geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=los angeles&limit=1&appid=b28ec9be3d4c68ba6aad2fa08ac12ced'
 
-request({url:url},(error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url:geoUrl, json:true},(error,response) => {
+    const [lat,lon] = [response.body[0].lat,response.body[0].lon]
+    console.log(lat,lon)
+    fetchWeather(lat,lon)
 })
+
+function fetchWeather(lat,lon){
+    const url = `http://api.weatherstack.com/current?access_key=ea797f0c26e77f60ee87ae3b81fd4f49&query=${lat},${lon}&units=f`
+
+    request({url:url, json:true},(error, response) => {
+        //const data = JSON.parse(response.body) since json:true it will be automatically an object/ parsed object
+        displayWeather(response.body.current)
+    })
+}
+
+function displayWeather(data){
+    //console.log(`The temperature is ${data.temperature} degree and it feels like ${data.feelslike} degree`)
+    console.log(data.weather_descriptions[0]) 
+    console.log(`The temperature is ${data.temperature} fahrenheit and it feels like ${data.feelslike} fahreinheit`)
+}
+
