2b953e1a3d7f05b52b941e42b1c23cc856d3b435
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2025-09-19 16:43:08 +0530

    error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,23 +3,32 @@ const request = require('postman-request');
 const geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=los angeles&limit=1&appid=b28ec9be3d4c68ba6aad2fa08ac12ced'
 
 request({url:geoUrl, json:true},(error,response) => {
-    const [lat,lon] = [response.body[0].lat,response.body[0].lon]
-    console.log(lat,lon)
-    fetchWeather(lat,lon)
+    
+    if(error){
+        console.log('Unable to connect to weather service')
+    } else {
+        const [lat,lon,name] = [response.body[0].lat,response.body[0].lon, response.body[0].name]
+        //console.log(lat,lon,name)
+        fetchWeather(lat,lon,name)
+    }    
 })
 
-function fetchWeather(lat,lon){
+function fetchWeather(lat,lon,name){
     const url = `http://api.weatherstack.com/current?access_key=ea797f0c26e77f60ee87ae3b81fd4f49&query=${lat},${lon}&units=f`
 
     request({url:url, json:true},(error, response) => {
-        //const data = JSON.parse(response.body) since json:true it will be automatically an object/ parsed object
-        displayWeather(response.body.current)
+        if(error){
+            console.log('Unable to connect to weather service')
+        } else {
+            //const data = JSON.parse(response.body) since json:true it will be automatically an object/ parsed object
+            displayWeather(response.body.current,name)
+        }
     })
 }
 
-function displayWeather(data){
+function displayWeather(data,name){
     //console.log(`The temperature is ${data.temperature} degree and it feels like ${data.feelslike} degree`)
     console.log(data.weather_descriptions[0]) 
-    console.log(`The temperature is ${data.temperature} fahrenheit and it feels like ${data.feelslike} fahreinheit`)
+    console.log(`The temperature of ${name} is ${data.temperature} fahrenheit and it feels like ${data.feelslike} fahreinheit`)
 }
 
