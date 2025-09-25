ead87484a1d6ff22c8d317736a82bff16c2e8773
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2025-09-25 15:03:54 +0530

    sending json data as response

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -13,10 +13,10 @@ const app = express() //express function doesn't take any argument
 
 //get method to send html or json
 
-app.get('',(req, res) => {  //2 arguments - url, function - what to do when someone visit this route
+app.get('',(req, res) => {  //2 arguments to callback function - url, function - what to do when someone visit this route
     //req is an object - contains info about incoming request
     //res - response has methods allows to customise what we send back to requester
-    res.send('Hello Express')
+    res.send('<h1>Weather</h1>')
 })
 
 /* on localhost:3000 it went to the server and the express server found the
@@ -24,15 +24,34 @@ matching route (root) and processed the request using handler
 and it sends response 'Hello Express' */
 
 app.get('/help',(req, res) => {
-    res.send('Help page')
+    res.send([
+        { //sending an array of objects - express automatically stringifies to JSON
+            name:'Aishwarya',
+            age:25
+        },
+        {
+            name: 'xyz'
+        }
+    ])
 })
 
 app.get('/about', (req, res) => {
-    res.send('Learn more about us')
+    res.send('<h1>Learn more about us</h1>')
 })
 
-app.get('/weather', (req, res) => {
-    res.send('Displaying weather')
+app.get('/weather', (req, res) => { //frontend app will get data from backend using this
+    res.send([
+        {
+            location:'Mumbai',
+            temperature:34,
+            forecast:'Hot and Humid'
+        },
+        {
+            location:'Thane',
+            temperature:30,
+            forecast:'Humid'
+        }
+    ])
 })
 
 //to start server
