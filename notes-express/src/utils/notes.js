855d41fa7f5a2c0f8b9cbaab9aece2b94066d0fb
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2025-09-29 14:42:05 +0530

    notes utils

---
+++ notes-express/src/utils/notes.js
@@ -0,0 +1,69 @@
+const fs = require('fs')
+const path = require('path')
+
+// Load notes
+function loadNotes() {
+    try {
+        const data = fs.readFileSync(filePath);
+        return JSON.parse(data);
+    } catch (err) {
+        return [];
+    }
+}
+
+// Save notes
+function saveNotes(notes) {
+    fs.writeFileSync(filePath, JSON.stringify(notes, null, 2));
+}
+
+// Add note
+function addNote(title, description, details) {
+    const notes = loadNotes();
+    const duplicate = notes.find(note => note.title === title);
+
+    if (duplicate) {
+        return { error: 'Title already exists!' };
+    }
+
+    notes.push({ title, description, details });
+    saveNotes(notes);
+    return { success: 'Note added successfully!' };
+}
+
+// Delete note
+function deleteNote(title) {
+    const notes = loadNotes();
+    const filtered = notes.filter(note => note.title !== title);
+
+    if (filtered.length === notes.length) {
+        return { error: 'Note not found!' };
+    }
+
+    saveNotes(filtered);
+    return { success: 'Note deleted successfully!' };
+}
+
+function readNote(title){
+    const notes = loadNotes();
+    const note = notes.find(note => note.title === title);
+
+    if (!note) {
+        return { error: 'Note not found!' };
+    }
+
+    return note; // return the found note object
+
+}
+
+//All notes
+function getNotes(){
+    return loadNotes();
+}
+
+
+module.exports = {
+    addNote,
+    deleteNote,
+    readNote,
+    getNotes
+}
