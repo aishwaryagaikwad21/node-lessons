05c4388ffdc1fbf4f2dc47df4882af6bd6f985b6
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2025-10-01 23:19:33 +0530

    delete notes

--- notes-express/public/js/client.js
+++ notes-express/public/js/client.js
@@ -19,7 +19,7 @@ const displayData = (data) => {
         const safeId = note.title.replace(/\s+/g, '-').toLowerCase();
 
         return `
-        <div class="accordion-item">
+        <div class="accordion-item" id="acc-item-${safeId}">
             <h2 class="accordion-header" id="heading-${safeId}">
                 <button class="accordion-button collapsed" type="button" 
                     data-bs-toggle="collapse" 
@@ -36,20 +36,37 @@ const displayData = (data) => {
                     <strong>${note.description}</strong>
                     <p>${note.details}</p>
                 </div>
-                <button type="button" class="btn btn-outline-primary" id="btn-upd-${safeId}" onclick="updateNote()">Update</button>
-                <button type="button" class="btn btn-outline-danger" id="btn-dng-${safeId}" onclick="deleteNote()" >Delete</button> <br>
+                <button type="button" class="btn btn-outline-primary" id="btn-upd-${safeId}" onclick="updateNote('${note.title}','${safeId}')">Update</button>
+                <button type="button" class="btn btn-outline-danger" id="btn-dng-${safeId}" onclick="deleteNote('${note.title}','${safeId}')" >Delete</button> <br> 
             </div>
         </div>
         `; }).join(''); //.join('') concatenates all array elements into a single string with no separator:
     notesContainer.innerHTML = notesHTML
 }
+//deleteNote('${note.title}')" --> note.title is a string and pass it with quotes
 
-const updateNote = () => {
-    console.log('Listening to update event')
+const updateNote = (title, safeId) => {
+    
+    const noteAcc = document.getElementById(`acc-item-${safeId}`)
+    console.log(noteAcc)
+    console.log('Listening to update event', title)
 }
 
-const deleteNote = () => {
-    console.log('Listening DELETE note event')
+const deleteNote = (title, safeId) => {
+    
+    fetch(`http://localhost:3000/notes/${encodeURIComponent(title)}`, {
+        method:'DELETE'
+    })
+    .then(response => {
+        if (!response.ok) throw new Error('Failed to delete note');
+        return response.json();
+    })
+    .then(data => {
+        console.log(data.message)
+        const noteAcc = document.getElementById(`acc-item-${safeId}`)
+        noteAcc.remove()
+    })
+    .catch(err => console.log('Error:', err))
 }
 
 const addNote = () => {
